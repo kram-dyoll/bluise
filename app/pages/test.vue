3799a52d2abc6d8d7789397fb319c37dd1ebb5aa
Mark Lloyd <m@splodge.co>
2022-01-29 15:59:35 +0000

    more fun test

---
+++ app/pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <button @click="functionTest">Fun times ahead</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    functionTest () {
+      //https://practical-shockley-347c4f.netlify.app/.netlify/functions/sharpspring-send
+      fetch('https://agitated-perlman-9f9465.netlify.app/.netlify/functions/sharpspring-send')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
